94ce7fb9e72865702fc4aa76ee4ae5330522205a
Marqui <98498681+Marqui-13@users.noreply.github.com>
2022-10-18 19:37:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -147,6 +147,25 @@ scene.add(camera);
 
 document.addEventListener("mousemove", animateParticles)
 
+// START JAVASCRIPT TOUCH EVENTS
+window.addEventListener("touchstart", event => {
+    mouse.x = event.changedTouches[0].clientX;
+    mouse.y = event.changedTouches[0].clientY;
+}, false);
+
+window.addEventListener("touchmove", event => {
+    //event.preventDefault();
+    mouse.x = event.targetTouches[0].clientX;
+    mouse.y = event.targetTouches[0].clientY;
+}, false);
+
+window.addEventListener("touchend", event => {
+    //event.preventDefault();
+    mouse.x = 0;
+    mouse.y = 0;
+}, false);
+// END JAVASCRIPT TOUCH EVENTS
+
 let mouseX = 0;
 let mouseY = 0;
 
